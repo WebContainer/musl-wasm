e828cb9fa5a04f87e5ed21fd8c97d65900f96cee
Jeremy Apthorp <nornagon@nornagon.net>
2018-05-14 17:26:28 -0700

    fix some stuff

--- arch/wasm32/atomic_arch.h
+++ arch/wasm32/atomic_arch.h
@@ -1,26 +1,18 @@
-#define a_ll a_ll
-extern int a_ll(volatile int *p);
+#ifndef __NR_restart_syscall
+#include <syscall.h>
+#endif
 
-#define a_sc a_sc
-extern int a_sc(volatile int *p, int v);
+#include <stdlib.h>
 
-#define a_barrier a_barrier
-extern void a_barrier();
+#define a_barrier() syscall(__NR_membarrier)
 
 #define a_cas a_cas
-extern int a_cas(volatile int *p, int t, int s);
+static inline int a_cas(volatile int *p, int t, int s)
+{
+  int old = *p;
+  if (old == t)
+    *p = s;
+  return old;
+}
 
-#define a_ll_p a_ll_p
-extern void *a_ll_p(volatile void *p);
-
-#define a_sc_p a_sc_p
-extern int a_sc_p(volatile int *p, void *v);
-
-#define a_cas_p a_cas_p
-extern void *a_cas_p(volatile void *p, void *t, void *s);
-
-#define a_ctz_64 a_ctz_64
-extern int a_ctz_64(uint64_t x);
-
-#define a_clz_64 a_clz_64
-extern int a_clz_64(uint64_t x);
+#define a_crash() abort()
--- arch/wasm32/syscall_arch.h
+++ arch/wasm32/syscall_arch.h
@@ -4,10 +4,10 @@
 #define __SYSCALL_LL_O(x) 0, __SYSCALL_LL_E((x))
 
 // https://en.wikipedia.org/wiki/Weak_symbol
-long __attribute__((weak)) __syscall0(long n);
-long __attribute__((weak)) __syscall1(long n, long a);
-long __attribute__((weak)) __syscall2(long n, long a, long b);
-long __attribute__((weak)) __syscall3(long n, long a, long b, long c);
-long __attribute__((weak)) __syscall4(long n, long a, long b, long c, long d);
-long __attribute__((weak)) __syscall5(long n, long a, long b, long c, long d, long e);
-long __attribute__((weak)) __syscall6(long n, long a, long b, long c, long d, long e, long f);
+long __syscall0(long n);
+long __syscall1(long n, long a);
+long __syscall2(long n, long a, long b);
+long __syscall3(long n, long a, long b, long c);
+long __syscall4(long n, long a, long b, long c, long d);
+long __syscall5(long n, long a, long b, long c, long d, long e);
+long __syscall6(long n, long a, long b, long c, long d, long e, long f);
